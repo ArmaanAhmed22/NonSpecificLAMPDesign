c18da13475dd1313803678bc567b900a97c81a58
Armaan Ahmed <aahmed22@scarsdaleschools.org>
2021-06-04 23:55:11 -0400

    Continuation option (not fully functional yet)

--- pipeline/run.py
+++ pipeline/run.py
@@ -8,12 +8,14 @@ def parse() -> tuple:
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-n", default=1, nargs="+",help="Number of iterations", type=int)
 	parser.add_argument("--inputs",nargs="+",type=str)
+	parser.add_argument("--continuation",type=bool,default=False)
 	args = parser.parse_args()
+
 
 	if args.inputs == None:
 		raise Exception("Invalid inputs")
 
-	files_len = len(args.input)
+	files_len = len(args.inputs)
 	generations = []
 	for n in args.n:
 		if n <= 0:
@@ -21,13 +23,13 @@ def parse() -> tuple:
 		else:
 			generations.append(n)
 	if len(generations) == 1:
-		generations = generations * len(files)
+		generations = generations * files_len
 	elif len(generations) != files_len:
 		raise Exception("Mismatch 'n' and 'inputs' sizes")
 
 	files = [File.construct_file(file_name,max_gen) for file_name,max_gen in zip(args.inputs,generations)]
 
-	return files
+	return (files,args.continuation)
 
 class File:
 	PARITY_IDENTIFIER = "_rev"
@@ -42,23 +44,29 @@ class File:
 		self._maximum_generation = maximum_generation
 
 	def get_full_name(self):
-		return self.base_name + f"_{self.number}" if self.parity else self.base_name + f"_rev" + f"_{self.number}"
+		return self.base_name + f"_{self.number}" if self.parity else self.base_name  + f"_{self.number}" + File.PARITY_IDENTIFIER
 
 	def get_double_negative_full_name(self):
 		if not self.parity:
-			raise Exception("parity needs to be positive for this method!")
-		return self.base_name + "_rev_rev" + f"_{self.number}"
+			raise Exception("parity needs to be True for this method!")
+		return self.base_name + f"_{self.number}" + File.PARITY_IDENTIFIER+File.PARITY_IDENTIFIER
 
 	def get_expanded_full_name(self):
-		return get_full_name() if self.parity else get_double_negative_full_name()
+		return self.get_full_name() if not self.parity else self.get_double_negative_full_name()
 
+	def file_increment(self,change:int):
+		return File(self.parity,self.base_name,self.number+change,self.generation,self.maximum_generation)
+	
 	@staticmethod
 	def construct_file(full_name:str,maximum_generation:int):
-		match_object = re.search("_\d+$",full_name)
-		if match_object is not None:
-			result = match_object.span()
-			is_rev = len(full_name) - (result[1] - result[0]) >= len(File.PARITY_IDENTIFIER) and full_name[result[0] - len(File.PARITY_IDENTIFIER):result[0]] == File.PARITY_IDENTIFIER
-			return File(not is_rev, full_name[:result[0] - len(File.PARITY_IDENTIFIER)] if is_rev else full_name[:result[0]],int(full_name[result[0]+1:result[1]]),1,maximum_generation)
+		match_object_fr = re.search("_\d+$",full_name)
+		match_object_rev = re.search("_\d+"+File.PARITY_IDENTIFIER+"$",full_name)
+		if match_object_fr is not None:
+			result = match_object_fr.span()
+			return File(True, full_name[:result[0]],int(full_name[result[0]+1:result[1]]),1,maximum_generation)
+		elif match_object_rev:
+			result = match_object_rev.span()
+			return File(False,full_name[:result[0]],int(full_name[result[0]+1:result[1] - len(File.PARITY_IDENTIFIER)]),1,maximum_generation)
 		else:
 			raise Exception("Incorrect file name")
 
@@ -69,18 +77,33 @@ class File:
 	def snakemake_construct_opposites(files,maximum_cores=4):
 		cores_used = min(maximum_cores,len(files))
 		new_files = [file.create_opposite() for file in files]
-		command = "snakemake -j" + str(cores_used) + " --use-conda ../Input/{" + ",".join([file.get_expanded_full_name() for file in new_files]) + "}/quasispecies.fasta"
-		os.system(command)
+		brace_begin = "{" if len(files) > 1 else ""
+		brace_end = "}" if len(files) > 1 else ""
+		command = "snakemake -j" + str(cores_used) + " --use-conda ../Input/"+brace_begin + ",".join([file.get_expanded_full_name() for file in new_files]) + brace_end+"/quasispecies.fasta"
+		assert os.system(command) == 0
 		for file in new_files:
 			if file.parity:
-				os.system(f"mv ../Input/{file.get_double_negative_full_name()} ../Input/{file.get_full_name()}")
+				assert os.system(f"mv ../Input/{file.get_double_negative_full_name()} ../Input/{file.get_full_name()}") == 0
 		return new_files
 
 	@staticmethod
 	def snakemake_run(files_total,maximum_cores=4):
-		cores_used = min(maximum_cores,len(files))
-		command = "snakemake -j" + str(cores_used) + " --use-conda ../Output/{" + ",".join([file.get_full_name() for file in files_total]) + "}/ready_primer_sets.json"
-		os.system(command)
+		cores_used = min(maximum_cores,len(files_total))
+		brace_begin = "{" if len(files_total) > 1 else ""
+		brace_end = "}" if len(files_total) > 1 else ""
+		command = "snakemake -j" + str(cores_used) + " --use-conda ../Output/"+brace_begin + ",".join([file.get_full_name() for file in files_total]) + brace_end+"/untargetable.fasta"
+		print(command)
+		assert os.system(command) == 0
+
+	@staticmethod
+	def create_next_files(files):
+		new_files = []
+		for file in files:
+			new_file = File(file.parity,file.base_name,file.number+1,file.generation+1,file._maximum_generation)
+			command = "mkdir ../Input/" + new_file.get_full_name() + "; cp ../Output/"+file.get_full_name()+"/untargetable.fasta ../Input/"+new_file.get_full_name()+"/quasispecies.fasta; cp ../Input/" + file.get_full_name() + "/reference.fasta ../Input/"+new_file.get_full_name()+"/reference.fasta"
+			assert os.system(command) == 0
+			new_files.append(new_file)
+		return new_files
 
 	@staticmethod
 	def evaluate_and_create_next_files_if_possible(files,files_opposite):
@@ -91,17 +114,19 @@ class File:
 				chosen = file_pair[1]
 				unchosen = file_pair[0]
 			else:
+				if lengths[0] == lengths[1] and lengths[1] == 0:
+					continue
 				chosen = file_pair[0]
 				unchosen = file_pair[1]
 
 			put_away_unnecessary = "mv ../Output/"+unchosen.get_full_name()+" ../Output/"+unchosen.get_full_name()+"_UNSELECTED"
 			if file_pair[0]._maximum_generation == file_pair[0].generation:
-				os.system(put_away_unnecessary)
+				assert os.system(put_away_unnecessary) == 0
 				continue
 			new_file = File(chosen.parity,chosen.base_name,chosen.number+1,chosen.generation+1,chosen._maximum_generation)
 			command = "mkdir ../Input/" + new_file.get_full_name() + "; cp ../Output/"+chosen.get_full_name()+"/untargetable.fasta ../Input/"+new_file.get_full_name()+"/quasispecies.fasta; cp ../Input/" + chosen.get_full_name() + "/reference.fasta ../Input/"+new_file.get_full_name()+"/reference.fasta"
-			os.system(command)
-			os.system(put_away_unnecessary)
+			assert os.system(command) == 0
+			assert os.system(put_away_unnecessary) == 0
 			new_files.append(new_file)
 		return new_files
 
@@ -112,10 +137,14 @@ class File:
 
 
 
-files = parse()
+files,cont = parse()
 
+if cont:
+	files = File.create_next_files(files)
 
 while len(files) >= 1:
+	for f in files:
+		print(f)
 	opposites = File.snakemake_construct_opposites(files)
 	File.snakemake_run(files+opposites)
 	files = File.evaluate_and_create_next_files_if_possible(files,opposites)
